6ee0166e4834f78b2af2990b4b3298524fce2424
safiqu9101 <137719089+safiqu9101@users.noreply.github.com>
2023-07-28 23:04:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/taitangamer/TaitanXMusic-Op",
+    "https://github.com/safiqu9101/anonx",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
